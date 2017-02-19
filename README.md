05d7d8427cde750a7f36c429ab72db02e74a0b61
nladuo <kalen25115@gmail.com>
2017-02-19 23:10:01 +0800

    get basic parameters.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # visit_analytics
-自用的网站访问量统计
+自用的网站访问量统计工具
 ## STATUS
 正在编写中...
 ## LICENSE
--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/analytics.js", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"success": true,
-		})
+		analyse(c)
+		c.Header("Content-Type", "application/javascript")
+		c.String(http.StatusOK, "console.log(\"https://github.com/nladuo/visit_analytics\")")
 	})
+
+	router.StaticFile("/index", "./www/index.html")
+
 	router.Run(":3000")
+}
+
+func analyse(c *gin.Context) {
+	if len(c.Request.Referer()) == 0 {
+		return
+	}
+	fmt.Println("ClientIp:", c.ClientIP())
+	fmt.Println("User-Agent:", c.Request.UserAgent())
+	fmt.Println("Referer:", c.Request.Referer())
+	fmt.Println("Title:", getTitle(c.Request.Referer()))
+}
+
+func getTitle(url string) string {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return url
+	}
+	title := doc.Find("title").Text()
+	title = strings.Trim(title, " ")
+	if len(title) == 0 {
+		return url
+	}
+	return title
 }
